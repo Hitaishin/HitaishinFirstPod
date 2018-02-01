b83e9c3bebadeddb06cc6e8dbdc0916a9080c254
Nishad Sheikh <nishad@nishads-mac-mini-4.local>
2018-02-01 12:34:02 +0530

    Initial Commit

---
+++ HitaishinFirstPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitaishinFirstPod
+//
+//  Created by Nishad Sheikh on 01/02/18.
+//  Copyright © 2018 Nishad Sheikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
